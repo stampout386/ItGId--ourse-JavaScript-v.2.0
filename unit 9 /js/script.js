942856d96b828dffd0b8996948ddee49c9c8c237
Mikhail Shestak <stampout386@gmail.com>
2021-11-12 20:11:47 +0300

    unit 9

---
+++ unit 9 /js/script.js
@@ -0,0 +1,36 @@
+const one = document.querySelector('.one');
+one.style.width = '150px';
+one.style.paddingBottom = '40px';
+
+console.log(one.style);
+
+one.classList.add('two','three'); // добавить любой цсс класс
+one.classList.remove('three');
+
+const toggle = document.querySelector('.toggle');
+
+toggle.onclick = function(){
+    this.classList.toggle('three');
+}
+console.log(one.getAttribute('data'));
+console.log(document.querySelector('link').getAttribute('href'));
+
+one.setAttribute('data-num', 6);
+document.querySelector('hr').style.width = '500px';
+
+let gas = document.querySelectorAll('.gas');
+for (let i = 0; i < gas.length; i++) {
+    gas[i].onclick = function(){
+        let gallons = document.querySelector('.gallons').value;
+        let amount = this.getAttribute('data');
+
+        console.log(gallons*amount);
+    };
+    
+}
+
+let a = document.createElement('div'); 
+console.log(a);
+a.innerHTML = 'Hello gays';
+a.classList.add('one');
+document.querySelector('.test').appendChild(a);
